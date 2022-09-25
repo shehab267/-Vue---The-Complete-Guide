c30910509ce2db85d6c1fb1697c79d76e1eea519
Shehab Alhawari <shehabelhawary267.com>
2022-09-25 17:39:41 +0200

    Adding logic to vue app

---
+++ lec 07/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>Friends</h2>
+  <ul>
+    <friend-contact></friend-contact>
+    <friend-contact></friend-contact>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "Manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 4567 7889",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julia",
+          name: "julia Smith",
+          phone: "0 952 5567 7889",
+          email: "julia@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ lec 07/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetais">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 4567 7889",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetais() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
