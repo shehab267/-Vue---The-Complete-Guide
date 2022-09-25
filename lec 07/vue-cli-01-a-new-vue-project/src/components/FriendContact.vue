9d23f87687afb842fb23d32cfd15218ea55c95e3
Shehab Alhawari <shehabelhawary267.com>
2022-09-25 17:52:58 +0200

    Styling the app

--- lec 07/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ lec 07/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,7 +1,9 @@
 <template>
   <li>
     <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetais">Show Details</button>
+    <button @click="toggleDetais">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone:</strong>{{ friend.phone }}</li>
       <li><strong>Email:</strong>{{ friend.email }}</li>
