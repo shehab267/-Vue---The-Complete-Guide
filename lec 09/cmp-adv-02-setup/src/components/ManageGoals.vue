ff47e80cb81e104194445fddce9650eca97c0386
Shehab Alhawari <shehabelhawary267.com>
2022-10-05 11:43:02 +0200

    Applying What we know

---
+++ lec 09/cmp-adv-02-setup/src/components/ManageGoals.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>Manage Goals</h2>
+  <div>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h3>Input Is Invalid</h3>
+      <p>Please enter a GOAL</p>
+      <button @click="closeError">Okay</button>
+    </error-alert>
+  </div>
+</template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enterdValue = this.$refs.goal.value;
+      if (enterdValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    closeError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
