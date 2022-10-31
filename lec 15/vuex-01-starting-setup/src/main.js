04082cfbbe23c3f3d6fec89d67dc4d2e9d2c8862
shehab267 <shehabelhawary267@gmail.com>
2022-10-31 09:05:34 +0200

    enhance the code

--- lec 15/vuex-01-starting-setup/src/main.js
+++ lec 15/vuex-01-starting-setup/src/main.js
@@ -17,6 +17,11 @@ const store = createStore({
       state.counter = state.counter + payload.value;
     },
   },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+  },
 });
 
 const app = createApp(App);
