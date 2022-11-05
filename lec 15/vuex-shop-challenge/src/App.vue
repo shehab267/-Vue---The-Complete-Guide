2a66333aa4e6b38842e050e12e0467890a27f607
shehab267 <shehabelhawary267@gmail.com>
2022-11-05 18:37:49 +0200

    Finish step One

--- lec 15/vuex-shop-challenge/src/App.vue
+++ lec 15/vuex-shop-challenge/src/App.vue
@@ -8,56 +8,24 @@ import TheHeader from './components/nav/TheHeader.vue';
 
 export default {
   components: {
-    TheHeader
+    TheHeader,
   },
   data() {
     return {
       isLoggedIn: false,
-      cart: { items: [], total: 0, qty: 0 },
     };
   },
-  provide() {
-    return {
-      isLoggedIn: this.isLoggedIn,
-      products: this.products,
-      cart: this.cart,
-      addProductToCart: this.addProductToCart,
-      removeProductFromCart: this.removeProductFromCart,
-      login: this.login,
-      logout: this.logout,
-    };
-  },
+  // provide() {
+  //   return {
+  //     isLoggedIn: this.isLoggedIn,
+  //     cart: this.cart,
+  //     addProductToCart: this.addProductToCart,
+  //     removeProductFromCart: this.removeProductFromCart,
+  //     login: this.login,
+  //     logout: this.logout,
+  //   };
+  // },
   methods: {
-    addProductToCart(productData) {
-      const productInCartIndex = this.cart.items.findIndex(
-        (ci) => ci.productId === productData.id
-      );
-
-      if (productInCartIndex >= 0) {
-        this.cart.items[productInCartIndex].qty++;
-      } else {
-        const newItem = {
-          productId: productData.id,
-          title: productData.title,
-          image: productData.image,
-          price: productData.price,
-          qty: 1,
-        };
-        this.cart.items.push(newItem);
-      }
-      this.cart.qty++;
-      this.cart.total += productData.price;
-    },
-
-    removeProductFromCart(prodId) {
-      const productInCartIndex = this.cart.items.findIndex(
-        (cartItem) => cartItem.productId === prodId
-      );
-      const prodData = this.cart.items[productInCartIndex];
-      this.cart.items.splice(productInCartIndex, 1);
-      this.cart.qty -= prodData.qty;
-      this.cart.total -= prodData.price * prodData.qty;
-    },
     login() {
       this.isLoggedIn = true;
     },
