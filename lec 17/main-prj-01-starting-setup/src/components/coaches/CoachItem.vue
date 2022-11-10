57e6a5e1a940f10a62da386858718031920c5bd6
shehab267 <shehabelhawary267@gmail.com>
2022-11-10 21:07:28 +0200

    Prop CaochItem

---
+++ lec 17/main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>{{ rate }}</h4>
+    <div>
+      <span
+        v-for="area in areas"
+        :key="area"
+        :id="area.id"
+        :first-name="firstName"
+        :last-name="lastName"
+        :hourly-rate="rate"
+        >{{ area }}</span
+      >
+    </div>
+    <div class="actions">
+      <router-link to="coachContactLink">Contact</router-link>
+      <router-link to="coachDetailsLink">Show Details</router-link>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+  //  need to pass data to specific throw props, BECAUSE data for specific coach is NOT managed with Vuex, it's just internal local state
+  props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    coachContactLink() {
+      return `${this.$route.path}/${this.id}/contact`;
+    },
+    coachDetailsLink() {
+      return `${this.$route.path}/${this.id}`;
+    },
+  },
+};
+</script>
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
