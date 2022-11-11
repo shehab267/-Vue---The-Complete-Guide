3881ccc6831b0cc2073753482e04487e61409ddf
shehab267 <shehabelhawary267@gmail.com>
2022-11-11 15:51:19 +0200

    Base Components

--- lec 17/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ lec 17/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,24 +1,27 @@
 <template>
-  <section><h3>FILTER</h3></section>
+  <base-card>
+    <section><h3>FILTER</h3></section>
+  </base-card>
   <section>
-    <!-- <h3>COACHES LIST</h3> -->
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :areas="coach.areas"
-        :rate="coach.hourlyRate"
-      >
-      </coach-item>
-    </ul>
-    <p v-else>No Coaches Found!</p>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline"> Refresh </base-button>
+        <base-button link to="/register">Register as a Coach </base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :areas="coach.areas"
+          :rate="coach.hourlyRate"
+        >
+        </coach-item>
+      </ul>
+      <p v-else>No Coaches Found!</p>
+    </base-card>
   </section>
 </template>
 
