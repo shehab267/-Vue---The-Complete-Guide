7d333d43d6344c91c1001cffa1d26da82b27ea20
shehab267 <shehabelhawary267@gmail.com>
2022-11-12 09:22:20 +0200

    Registaring as a Coach

--- lec 17/main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
+++ lec 17/main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,16 @@
-<template>Register</template>
+<template>
+  <base-card>
+    <h2>Register as a coach now!</h2>
+    <CoachFormVue></CoachFormVue
+  ></base-card>
+</template>
+
+<script>
+import CoachFormVue from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachFormVue,
+  },
+};
+</script>
