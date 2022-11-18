dc31550cf3253fef41a9ab53a8dc63eef3880b99
shehab267 <shehabelhawary267@gmail.com>
2022-11-18 10:23:23 +0200

    Adding Loading Spinner Logic

--- lec 17/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ lec 17/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -6,11 +6,14 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register"
+        <base-button v-if="!isCoach && !isLoading" link to="/register"
           >Register as Coach</base-button
         >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -29,6 +32,7 @@
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
 import CoachFilter from '../../components/coaches/CoachFilter.vue';
+
 export default {
   components: {
     CoachItem,
@@ -36,6 +40,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       activeFilter: {
         frontend: true,
         backend: true,
@@ -69,7 +74,7 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
   },
   methods: {
@@ -77,8 +82,10 @@ export default {
     setFilter(updatedFilter) {
       this.activeFilter = updatedFilter;
     },
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches');
+    async loadCoaches() {
+      this.isLoading = true;
+      await this.$store.dispatch('coaches/loadCoaches');
+      this.isLoading = false;
     },
   },
 };
