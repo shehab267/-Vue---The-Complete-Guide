f3d9a6a3300cb0833128d4a76e2199a87c7ef02f
shehab267 <shehabelhawary267@gmail.com>
2022-11-21 20:45:14 +0200

    Sending Coaching Requests Http Requests

--- lec 17/main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ lec 17/main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -1,11 +1,25 @@
 export default {
   contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
       coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+
+    // Post requests on dataBase based on the CoachId
+    const response = fetch(
+      `https://finding-coach-web-app-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    // const responseData = response.json;
+    if (!response.ok) {
+      // Do something
+    }
+
     // commit addRequests mutation and pass the payload 'newRequest'
     context.commit('addRequests', newRequest);
   },
