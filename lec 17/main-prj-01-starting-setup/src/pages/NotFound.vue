2e46f00f9f928f4ffac878420cebe541b8cafae7
shehab267 <shehabelhawary267@gmail.com>
2022-11-25 21:04:34 +0200

    NotFound Page

--- lec 17/main-prj-01-starting-setup/src/pages/NotFound.vue
+++ lec 17/main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -1,11 +1,12 @@
 <template>
-  <h2>
-    Page not found! Maybe view our <router-link to="/coaches">Coaches List</router-link>?
-  </h2>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>.
+      </p>
+    </base-card>
+  </section>
 </template>
 
-<style scoped>
-h2 {
-  margin: 20px 10px;
-}
-</style>
