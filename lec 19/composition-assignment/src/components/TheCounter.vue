511de25ce99f3ae71739091647775cd71fc5d6e3
shehab267 <shehabelhawary267@gmail.com>
2023-02-07 14:02:09 +0200

    Vuex with Composition API

---
+++ lec 19/composition-assignment/src/components/TheCounter.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>{{ counter }}</h2>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+const store = useStore();
+
+const counter = computed(function () {
+  return store.getters.counter;
+});
+</script>
